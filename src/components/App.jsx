34317d9ef6716fc074b991833b880cdef89e3904
Gian Nathan Christyo Nugroho <78425787+giethn@users.noreply.github.com>
2023-09-23 12:41:40 +0700

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import Header from './Header';
+import Form from './Form';
+import GroceryList from './GroceryList';
+import Footer from './Footer';
+
+const groceryItems = [
+  {
+    id: 1,
+    name: 'Kopi Bubuk',
+    quantity: 2,
+    checked: true,
+  },
+];
+
+export default function App() {
+  const [items, setItems] = useState(groceryItems);
+
+  function handleAddItem(item) {
+    setItems([...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) => items.map((item) => (item.id === id ? { ...item, checked: !item.checked } : item)));
+  }
+
+  function handleClearItems() {
+    setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Header />
+      <Form onAddItem={handleAddItem} />
+      <GroceryList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} onClearItems={handleClearItems} />
+      <Footer items={items} />
+    </div>
+  );
+}
